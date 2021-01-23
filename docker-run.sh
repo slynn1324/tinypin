4bf7727ddd10c1867202cf3e64a1de27c1658a64
slynn1324 <slynn1324@gmail.com>
2021-01-23 14:07:13 -0600

    updated docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #-e TINYPIN_SLOW=2000
-docker run --name tinypin -p 3001:3000 -v "$(pwd)/data:/data" tinypin
+docker run -d --name tinypin -p 3001:3000 -v "$(pwd)/data:/data" --restart=unless-stopped tinypin
