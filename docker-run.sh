cd5dee3d6d42037cd501937c6b7b1c5de91c1758
slynn1324 <slynn1324@gmail.com>
2021-01-25 12:35:42 -0600

    update docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 #-e TINYPIN_SLOW=2000
-docker run -d --name tinypin -p 3001:3000 -v "/mnt/tank/tinypin-data:/data" --restart=unless-stopped tinypin
+docker run -d \
+    --name tinypin \
+    -p 3001:3000 \
+    -v "/mnt/tank/tinypin-data:/data" \
+    --restart=unless-stopped \
+    slynn1324/tinypin
